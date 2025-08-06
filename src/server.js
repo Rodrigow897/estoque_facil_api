991e6fb03452fe490420a0433a8840a249dbdc6a
Rodrigo <rwesley2000@gmail.com>
2025-08-06 10:44:22 -0300

    refactor: otimiza a função de codigo

--- src/server.js
+++ src/server.js
@@ -1,4 +1,15 @@
-const app = require('./app');
+const express = require('express');
+const app = express();
+const produtosRoutes = require('./routes/produtosRoutes');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('bem vindo ao Estoque Facil API');
+});
+
+app.use('/produtos', produtosRoutes);
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
